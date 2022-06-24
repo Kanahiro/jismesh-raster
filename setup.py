3032282738d33ddcb0ff2618cd4e157563f5860d
Kanahiro <mediterranean1769@gmail.com>
2022-06-24 23:27:52 +0900

    v0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jismesh-raster",
-    version="0.2.0",
+    version="0.3.0",
     description="Generate raster from jismesh-based data. jismesh=Japan Standard Mesh",
     author="Kanahiro Iguchi",
     license="MIT",
@@ -13,5 +13,5 @@ setup(
         "console_scripts": [
             "jismesh-raster=jismesh_raster.rasterize:main",
         ]
-    }
+    },
 )
